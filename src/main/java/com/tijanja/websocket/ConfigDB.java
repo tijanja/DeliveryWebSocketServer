29fc6373c10187b7e28b9e5875153dc045c8df35
tijanja007 <adetunji.akinde@techadvance.ng>
2019-12-12 21:00:47 +0100

    change mainApp name

--- src/main/java/com/tijanja/websocket/ConfigDB.java
+++ src/main/java/com/tijanja/websocket/ConfigDB.java
@@ -7,17 +7,15 @@ import java.sql.SQLException;
 
 public class ConfigDB {
     
-    public static Connection connectDB()
-    {
+    
+
+    public static Connection connectDB() {
         Connection con = null;
-        try
-        { 
-                Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-                con=DriverManager.getConnection("jdbc:mysql://206.189.115.158:3306/dinkkyDB","tijanja","Project123"); 
-                //con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/dinkkyDB","root","Project123"); 
-        }
-        catch(SQLException e)
-        {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://206.189.115.158:3306/dinkkyDB", "tijanja", "Project123");
+            // con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/dinkkyDB","root","Project123");
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
             
         }
