9c514fe46b88e4aebc3855b7ab63184f491f96a8
Adetunji Akinde <adetunjiakinde@gmail.com>
2020-01-28 10:56:27 +0100

    Update ConfigDB.java
    edit database config file

--- src/main/java/com/tijanja/websocket/ConfigDB.java
+++ src/main/java/com/tijanja/websocket/ConfigDB.java
@@ -13,7 +13,7 @@ public class ConfigDB {
         Connection con = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-            con = DriverManager.getConnection("jdbc:mysql://localhost/dinkkyDB?serverTimezone=UTC", "admin", "Pr0j3ct1@e");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/dinkkyDB?serverTimezone=UTC", "", "");
             // con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/dinkkyDB","root","Project123");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
